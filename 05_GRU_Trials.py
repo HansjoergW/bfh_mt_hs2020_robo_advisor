8a247766c6e2476bad8e26aded4396b589b3b59b
Hansjoerg <hansjoeg.wingeier@gmail.com>
2021-01-17 09:05:46 +0100

    Gru_Model ready for training

--- 05_GRU_Trials.py
+++
@@ -1,4 +0,0 @@
-# https://stackabuse.com/time-series-prediction-using-lstm-with-pytorch-in-python/
-# https://www.programmersought.com/article/95354258342/
-# https://blog.floydhub.com/gru-with-pytorch/
-# https://www.youtube.com/watch?v=0_PgWWmauHk&t=12s
